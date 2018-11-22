e31193143de0e2865c8ed0a37e8185ba48090e09
NathanB09 <nathanbell09@gmail.com>
2018-11-22 12:35:31 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,5 @@
 class Artist
 
-  @@artists = []
   attr_accessor :name
-
-  def initialize
-    self.class.all << self
-  end
-
-  def self.all
-    @@artists
-  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,5 @@
 class Song
 
-  @@songs = []
-
   attr_accessor :title, :artist
-
-  def initialize
-    self.class.all << self
-  end
-
-  def self.all
-    @@songs
-  end
 
 end
